ce375e4387c790e5797b834ab80e6a88f322211e
HirakuShibuya <49626558+HirakuShibuya@users.noreply.github.com>
2020-02-23 11:17:42 +0900

    Update fileupload.py

--- SAC/sales_base_data/fileupload.py
+++ SAC/sales_base_data/fileupload.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 
+class fileupload:
+  def uploadFile(self):
 ############################################
 #売上情報をアップロード
 ############################################
 
-import pandas as pd
-import datetime
-import pandas.tseries.offsets as offsets
-from dateutil.relativedelta import relativedelta
-from google.colab import files
-import imageio
+    import pandas as pd
+    import datetime
+    import pandas.tseries.offsets as offsets
+    from dateutil.relativedelta import relativedelta
+    from google.colab import files
+    import imageio
 
-!rm -rf *.xlsx
+    !rm -rf *.xlsx
 
-inputs = []
-images = []
+    inputs = []
+    images = []
 
-uploaded = files.upload()
+    uploaded = files.upload()
 
-df=pd.read_excel(list(uploaded)[0],sheet_name="all")
+    df=pd.read_excel(list(uploaded)[0],sheet_name="all")
