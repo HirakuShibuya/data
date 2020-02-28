98486aaa3c8a494facdb9f4274a180d7d69400b1
HirakuShibuya <49626558+HirakuShibuya@users.noreply.github.com>
2020-02-28 17:49:03 +0900

    Update HTML.py

--- SAC/sales_base_data/HTML.py
+++ SAC/sales_base_data/HTML.py
@@ -16,7 +16,7 @@ class createHtml():
     return src2
   
   
-  def cHtml(self, dframe, dir):
+  def cHtml(dframe, dir):
     html='''
   <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
