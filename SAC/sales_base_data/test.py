e07db93e2ad12033bf89467904b6ebbc975581ba
HirakuShibuya <49626558+HirakuShibuya@users.noreply.github.com>
2020-03-04 16:02:25 +0900

    Update test.py

--- SAC/sales_base_data/test.py
+++ SAC/sales_base_data/test.py
@@ -2,7 +2,7 @@ class test_s:
   def __init__(self,sname='all'):
     self.sname = sname 
     
-  def print_name(sname):
+  def print_name(self, sname):
     print(sname)
     
   def print_name2(self,sname):
